5a4b19f1a7c80b5c061a21fe3014f1de19483670
Devin <devin@getcraft.com>
2018-10-16 11:30:48 +0700

    added testing async

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -37,3 +37,17 @@ it('should expect some values', () => {
     age: 25
   })
 })
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number')
+    done()
+  })
+})
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(5, (res) => {
+    expect(res).toBe(25).toBeA('number')
+    done()
+  })
+})
