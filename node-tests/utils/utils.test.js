872a7aa946895dcd5609d76a6381db7ea1d7e1df
Devin <devin@getcraft.com>
2018-10-19 11:19:41 +0700

    added testing db modification (test spies)

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,25 +2,30 @@ const expect = require('expect')
 
 const utils = require('./utils')
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11)
-  
-  expect(res).toBe(44).toBeA('number')
-})
+describe('Utils', () => {
 
-it('should square a number', () => {
-  var res = utils.square(3)
-  
-  expect(res).toBe(9).toBeA('number')
-})
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11)
 
-it('should verify first and last names are set', () => {
-  var user = {location: 'BSD', age: 24}
-  var res = utils.setName(user, 'Devin Ekadeni')
+      expect(res).toBe(44).toBeA('number')
+    })
+  })
 
-  expect(user).toInclude({
-    firstName: 'Devin',
-    lastName: 'Ekadeni',
+  it('should square a number', () => {
+    var res = utils.square(3)
+
+    expect(res).toBe(9).toBeA('number')
+  })
+
+  it('should verify first and last names are set', () => {
+    var user = { location: 'BSD', age: 24 }
+    var res = utils.setName(user, 'Devin Ekadeni')
+
+    expect(user).toInclude({
+      firstName: 'Devin',
+      lastName: 'Ekadeni',
+    })
   })
 })
 
