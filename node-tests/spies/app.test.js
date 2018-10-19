95ae37dc05e6656db641fb88d3ed556ba419a0ac
Devin <devin@getcraft.com>
2018-10-19 11:24:22 +0700

    added function testing

--- node-tests/spies/app.test.js
+++ node-tests/spies/app.test.js
@@ -1,9 +1,27 @@
 const expect = require('expect')
+const rewire = require('rewire')
+
+var app = rewire('./app')
 
 describe('App', () => {
+  var db = {
+    saveUser: expect.createSpy()
+  }
+
+  app.__set__('db', db)
   it('should call the spy correctly', () => {
     var spy = expect.createSpy()
-    spy()
-    expect(spy).toHaveBeenCalled()
+    // spy()
+    // expect(spy).toHaveBeenCalled()
+    spy('devin', 25)
+    expect(spy).toHaveBeenCalledWith('devin', 25)
+  })
+
+  it('should call saveuser with user object', () => {
+    var email = 'devin@gmail.com'
+    var password = 'devin12345'
+
+    app.handleSignUp(email, password)
+    expect(db.saveUser).toHaveBeenCalledWith({ email, password })
   })
 })
