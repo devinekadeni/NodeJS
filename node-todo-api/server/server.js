1138d3ea4907e1c782c5a1ea737f22927a8c90d8
Devin <devin@getcraft.com>
2018-10-21 18:33:34 +0700

    added API /todos and /user

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,28 +1,40 @@
-var mongoose = require('mongoose')
+var express = require('express')
+var bodyParser = require('body-parser')
 
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+var { mongoose } = require('./db/mongoose')
+var { Todo } = require('./models/todo')
+var { User } = require('./models/user')
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+var app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save().then(result => {
+    res.send(result)
+  })
+  .catch(e => {
+    res.status(400).send(e)
+  })
 })
 
-var newTodo = new Todo({
-  text: 'Cook dinner',
-  completed: false,
-  completedAt: 213123
+app.post('/user', (req, res) => {
+  var user = new User({
+    email: req.body.email
+  })
+
+  user.save().then(result => {
+    res.send(result)
+  })
+  .catch(e => {
+    res.status(400).send(e)
+  })
 })
 
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc)
-}).catch((e) => {
-  console.log('Unable to save todo')
+app.listen(3000, () => {
+  console.log('Started on port 3000')
 })
